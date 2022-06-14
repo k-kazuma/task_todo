192a04a4fd599ead4318f19697526e1573b7c6ee
k-kazuma <k_kazuma8@icloud.com>
2022-06-14 23:25:22 +0900

    ..

--- task.py
+++ task.py
@@ -11,17 +11,33 @@ class App:
         self.menu = Menu(self.master)
         self.menu.pack(side='top')
 
+        # 表示エリア
+        self.listarea = ListArea(self.master)
+        self.listarea.pack(side='top')
+
+        # 入力エリア
+        self.inputarea = InputArea(self.master)
+        self.inputarea.pack(side='top')
+        self.inputarea.click_add_btn = self.click_add_btn
+
     def mainloop(self):
         self.master.mainloop()
 
+    def click_add_btn(self):
+        todo = self.inputarea.entry.get()
+        self.inputarea.entry.delete(0, 'end')
+        self.listarea.listbox.insert('end', todo)
 
 
 class Menu(tk.Frame):
     def __init__(self, master):
         super(Menu, self).__init__(master)
 
-        self.menu_btn = tk.Button(self, text='test')
-        self.menu_btn.pack(side='top')
+        self.menu_btn = tk.Button(self, text='終了', command=self.end_button)
+        self.menu_btn.pack(side='right')
+
+    def end_button(self):
+        self.master.destroy()
 
         # ボタンを押した時の処理
 
@@ -31,6 +47,22 @@ class ListArea(tk.Frame):
 
         self.listbox = tk.Listbox(self, height=5)
         self.listbox.pack(side='top')
+
+class InputArea(tk.Frame):
+    def __init__(self, master):
+        super(InputArea, self).__init__(master)
+
+        self.click_add_btn = None
+
+        self.entry = tk.Entry(self)
+        self.entry.pack(side='left')
+
+        self.inputbtn = tk.Button(self, text='追加', command=self._click_add_btn)
+        self.inputbtn.pack(side='right', expand=True, fill='x')
+
+    def _click_add_btn(self):
+        if self.click_add_btn:
+            self.click_add_btn()
 
 def main():
     app = App()
